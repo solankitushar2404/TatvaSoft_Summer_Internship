ac9a5aaae5f39f2c05bad8089904c59f5c06de3a
Solanki Tusharkumar <solankitusharramjibhai@gmail.com>
2025-07-07 21:11:32 +0530

    Add Day 4 Folder With Mission And Frontend With OutputImages

---
+++ Day 4/Mission/Mission.Entities/MissionDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Mission.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Mission.Entities
+{
+    public class MissionDbContext : DbContext
+    {
+        public MissionDbContext(DbContextOptions<MissionDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                FirstName = "Admin",
+                LastName = "tatvasoft",
+                EmailAddress = "admin@tatvasoft.com",
+                Password = "admin",
+                PhoneNumber = "01234567890",
+                UserType = "admin",
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
---
+++ Day 4/Mission/Mission.Repositories/IRepository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Mission.Entities.ViewModels.Login;
+
+namespace Mission.Repositories.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<(UserLoginResponseModel? response, string message)> LogiUser(UserLoginRequestModel model);
+    }
+}
+
