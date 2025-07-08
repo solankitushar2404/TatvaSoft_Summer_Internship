1f57d8f747bd36672a939ac3e43ea07dfc44a093
Solanki Tusharkumar <solankitusharramjibhai@gmail.com>
2025-07-08 20:03:52 +0530

    Add Day 5 Folder With User CRUD Operation

--- Day 3/Mission/Mission.Entities/obj/Release/net8.0/Mission.Entities.AssemblyInfo.cs
+++ Day 3/Mission/Mission.Entities/obj/Release/net8.0/Mission.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb12d34d774cd8faa31e8506d9a6e4144ded7dfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
