0d88df7b108132bbfc8ef1e2836164f91bc085e4
Solanki Tusharkumar <solankitusharramjibhai@gmail.com>
2025-07-09 22:48:07 +0530

    Add Day 6 Folder And Mission Skill CRUD + Mission Theme CRUD Operation With OutputImages

---
+++ Day 6/Mission/Mission.Repositories/IRepository/IMissionThemeRepository.cs
@@ -0,0 +1,17 @@
+﻿using Mission.Entities.ViewModels.MissionTheme;
+
+namespace Mission.Repositories.IRepository
+{
+    public interface IMissionThemeRepository
+    {
+        Task AddMissionThemeAsync(UpsertMissionThemeRequestModel model);
+
+        Task<List<MissionThemeResponseModel>> GetMissionThemeListAsync();
+
+        Task<MissionThemeResponseModel?> GetMissionThemeByIdAsync(int missionThemeId);
+
+        Task<bool> UpdateMissionThemeAsync(UpsertMissionThemeRequestModel model);
+
+        Task<bool> DeleteMissionTheme(int id);
+    }
+}
