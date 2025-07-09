b03db2b6e2b0c41a4c540b64b0f15963bca16105
Solanki Tusharkumar <solankitusharramjibhai@gmail.com>
2025-07-09 22:45:01 +0530

    Remove Day 6 folder from repository

--- Day 6/Mission/Mission.Entities/ViewModels/MissionTheme/UpsertMissionThemeRequestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mission.Entities.ViewModels.MissionTheme
-{
-    public class UpsertMissionThemeRequestModel
-    {
-        public int Id { get; set; }
-        public string ThemeName { get; set; }
-        public string Status { get; set; }
-    }
-}
