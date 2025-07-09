8e007762af9428a6988d4bc9019f5e131c762548
Solanki Tusharkumar <solankitusharramjibhai@gmail.com>
2025-07-09 22:27:38 +0530

    Add Day 5 Folder And Mission Skill CRUD + Mission Theme CRUD Operation With OutputImages

---
+++ Day 6/Mission/Mission.Entities/ViewModels/MissionTheme/MissionThemeResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using Mission.Entities.ViewModels.MissionSkill;
+
+namespace Mission.Entities.ViewModels.MissionTheme
+{
+    public class MissionThemeResponseModel : UpsertMissionThemeRequestModel
+    {
+        public MissionThemeResponseModel() { }
+
+        public MissionThemeResponseModel(Models.MissionTheme missionTheme)
+        {
+            Id = missionTheme.Id;
+            ThemeName = missionTheme.ThemeName;
+            Status = missionTheme.Status;
+        }
+    }
+}
